17bbcb1d8b591ec24eea6456f7dd378c2e563a5a
gnmathew <genemathewdorado@gmail.com>
2024-10-01 12:16:28 +0800

    [ Add ] Domain constraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
+  constraints(ClientDomainConstraint.new) do
+    namespace :api do
+      namespace :v1 do
+        namespace :client do
+          resources :registrations, only: :create
+          resources :sessions, only: :create
+        end
+      end
+    end
+  end
 
-  namespace :api do
-    namespace :v1 do
-      namespace :client do
-        resources :registrations, only: :create
-        resources :sessions, only: :create
+  constraints(AdminDomainConstraint.new) do
+    namespace :api do
+      namespace :v1 do
+        namespace :admin do
+          resources :sessions, only: :create
+        end
       end
     end
   end
